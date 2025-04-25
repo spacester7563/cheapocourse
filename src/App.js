fec540e637e3bd9e9851ea39183357859a51931a
aismartcouse <aismartcouse@gmail.com>
2025-04-25 06:56:23 +0000

    Added RTL

--- src/App.js
+++ src/App.js
@@ -44,74 +44,89 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import { googleClientId } from './constants';
 import Shareable from './pages/shareable';
 import Exam from './pages/exam';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [isRTL, setIsRTL] = useState(false);
+
+  useEffect(() => {
+    if (isRTL) {
+      document.body.classList.add('rtl');
+    } else {
+      document.body.classList.remove('rtl');
+    }
+  }, [isRTL]);
+
+  const toggleRTL = () => {
+    setIsRTL(!isRTL);
+  };
 
   return (
     <GoogleOAuthProvider clientId={googleClientId}>
-    <Router>
-      <div>
-        <ToastContainer
-          limit={3}
-          progressClassName={sessionStorage.getItem('darkMode') === 'true' ? "toastProgressDark" : "toastProgress"}
-          bodyClassName={sessionStorage.getItem('darkMode') === 'true' ? "toastBodyDark" : "toastBody"}
-          position="bottom-center"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme={sessionStorage.getItem('darkMode') === 'true' ? 'dark' : 'light'}
-        />
-        <Routes>
-          {/* Main App */}
-          <Route path="/" exact element={<Landing />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path='/forgot' element={<ForgotPassword />} />
-          <Route path='/reset-password/:token' element={<ResetPassword />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/create' element={<Create />} />
-          <Route path='/topics' element={<Topics />} />
-          <Route path='/course' element={<Course />} />
-          <Route path='/certificate' element={<Certificate />} />
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/pricing' element={<Pricing />} />
-          <Route path='/features' element={<Features />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/payment' element={<Payment />} />
-          <Route path='/success' element={<Success />} />
-          <Route path='/subscription' element={<Manage />} />
-          <Route path='/successful' element={<Successful />} />
-          <Route path='/failed' element={<Failed />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/terms' element={<TermsPolicy />} />
-          <Route path='/privacy' element={<PrivacyPolicy />} />
-          <Route path='/cancellation' element={<CancelPolicy />} />
-          <Route path='/refund' element={<RefundPolicy />} />
-          <Route path='/billing' element={<BillingPolicy />} />
-          <Route path='/pending' element={<Pending />} />
-          <Route path='/shareable' element={<Shareable />} />
-          <Route path='/exam' element={<Exam />} />
-          {/* Admin Panel */}
-          <Route path='/dashBoard' element={<DashBoard />} />
-          <Route path='/users' element={<Users />} />
-          <Route path='/courses' element={<Courses />} />
-          <Route path='/paid' element={<PaidUsers />} />
-          <Route path='/admins' element={<Admins />} />
-          <Route path='/contacts' element={<Contacts />} />
-          <Route path='/editterms' element={<Terms />} />
-          <Route path='/editprivacy' element={<Privacy />} />
-          <Route path='/editcancellation' element={<Cancellation />} />
-          <Route path='/editrefund' element={<Refund />} />
-          <Route path='/editbilling' element={<Billing />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </div>
-    </Router>
+      <Router>
+        <div className={isRTL ? 'rtl' : ''}>
+          <button onClick={toggleRTL}>Toggle RTL</button>
+          <ToastContainer
+            limit={3}
+            progressClassName={sessionStorage.getItem('darkMode') === 'true' ? "toastProgressDark" : "toastProgress"}
+            bodyClassName={sessionStorage.getItem('darkMode') === 'true' ? "toastBodyDark" : "toastBody"}
+            position="bottom-center"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme={sessionStorage.getItem('darkMode') === 'true' ? 'dark' : 'light'}
+          />
+          <Routes>
+            {/* Main App */}
+            <Route path="/" exact element={<Landing />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path='/forgot' element={<ForgotPassword />} />
+            <Route path='/reset-password/:token' element={<ResetPassword />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/create' element={<Create />} />
+            <Route path='/topics' element={<Topics />} />
+            <Route path='/course' element={<Course />} />
+            <Route path='/certificate' element={<Certificate />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/pricing' element={<Pricing />} />
+            <Route path='/features' element={<Features />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/payment' element={<Payment />} />
+            <Route path='/success' element={<Success />} />
+            <Route path='/subscription' element={<Manage />} />
+            <Route path='/successful' element={<Successful />} />
+            <Route path='/failed' element={<Failed />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/terms' element={<TermsPolicy />} />
+            <Route path='/privacy' element={<PrivacyPolicy />} />
+            <Route path='/cancellation' element={<CancelPolicy />} />
+            <Route path='/refund' element={<RefundPolicy />} />
+            <Route path='/billing' element={<BillingPolicy />} />
+            <Route path='/pending' element={<Pending />} />
+            <Route path='/shareable' element={<Shareable />} />
+            <Route path='/exam' element={<Exam />} />
+            {/* Admin Panel */}
+            <Route path='/dashBoard' element={<DashBoard />} />
+            <Route path='/users' element={<Users />} />
+            <Route path='/courses' element={<Courses />} />
+            <Route path='/paid' element={<PaidUsers />} />
+            <Route path='/admins' element={<Admins />} />
+            <Route path='/contacts' element={<Contacts />} />
+            <Route path='/editterms' element={<Terms />} />
+            <Route path='/editprivacy' element={<Privacy />} />
+            <Route path='/editcancellation' element={<Cancellation />} />
+            <Route path='/editrefund' element={<Refund />} />
+            <Route path='/editbilling' element={<Billing />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </div>
+      </Router>
     </GoogleOAuthProvider>
   );
 }
